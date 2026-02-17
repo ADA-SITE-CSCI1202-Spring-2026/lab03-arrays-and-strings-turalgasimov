97e2a2d16ee562711dcc8870c1958c1d2c12c37b
turalgasimov <turallgasimov@gmail.com>
2026-02-17 10:43:43 +0400

    sort string

--- StringUtils.java
+++ StringUtils.java
@@ -23,8 +23,25 @@ public class StringUtils {
 		return sb.toString();
 	}
 
+	public static String Sort(String s) {
+		s = s.toLowerCase();
+		char[] chars = s.toCharArray();
+
+		for (int i = 0; i < chars.length - 1; i++) {
+			for (int j = 0; j < chars.length - i - 1; j++) {
+				if (chars[j] > chars[j + 1]) {
+					char temp = chars[j];
+					chars[j] = chars[j + 1];
+					chars[j + 1] = temp;
+				}
+			}
+		}
+		return new String(chars);
+	}
+
 	public static void main(String[] args) {
 		System.out.println(Reverse("Hello, World!"));
 		System.out.println(Explode("Baku"));
+		System.out.println(Sort("Baku"));
 	}
 }
